901bffc8fd8b09acfbf6d28d233d262a0dfe5cd5
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-05 15:02:42 +0900

    update

--- frontend/src/component/Appointment/AppointmentContainer.jsx
+++ frontend/src/component/Appointment/AppointmentContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AppointmentContainer = ({ children }) => {
   return (
     <>
-      <div className="my-4 lg:w-[60%] mx-auto">
+      <div className="my-4 lg:w-[70%] mx-auto">
         <section className="grid grid-cols-12 p-2 bg-white rounded-md font-bold text-xl">
           <p className="col-span-2">No.</p>
           <p className="col-span-4">Cus. Name</p>
