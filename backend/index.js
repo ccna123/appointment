0f9da1b845282a8213caac00b97e02df4d6835b3
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-17 17:15:12 +0900

    update

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,19 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const appointRoute = require('./routes/Appoint.js')
-const userRoute = require('./routes/User.js')
-const adminRoute = require('./routes/Admin.js')
+const appointRoute = require("./routes/Appoint.js");
+const userRoute = require("./routes/User.js");
+const adminRoute = require("./routes/Admin.js");
 app.use(cors());
 app.use(express.json());
-app.use("/appoint", appointRoute)
-app.use("/user", userRoute)
-app.use("/admin", adminRoute)
 
-
-
+app.get("/", (req, res) => {
+  res.set("Content-Type", "text/plain");
+  res.status(200).send("Server Healthy");
+});
+app.use("/appoint", appointRoute);
+app.use("/user", userRoute);
+app.use("/admin", adminRoute);
 
 app.listen(4000, () => {
   console.log("Listening port 4000");
