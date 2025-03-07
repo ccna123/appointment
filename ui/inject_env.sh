5bc924d27b6bd1e38e5322b470f87dff41c32d5c
ccna123 <nguyenthanh24494@gmail.com>
2025-03-07 10:45:15 +0900

    Fix indentation in inject_env.sh for better readability

--- ui/inject_env.sh
+++ ui/inject_env.sh
@@ -4,19 +4,19 @@ INDEX_HTML="/usr/share/nginx/html/index.html"
 
 # Check if the file exists
 if [ ! -f "$INDEX_HTML" ]; then
-  echo "Error: $INDEX_HTML not found!"
-  exit 1
+    echo "Error: $INDEX_HTML not found!"
+    exit 1
 fi
 
 # Inject environment variables into index.html
 for i in $(env | grep REACT_APP_); do
-  key=$(echo $i | cut -d '=' -f 1)
-  value=$(echo $i | cut -d '=' -f 2- | sed -e 's/[\/&]/\\&/g' -e 's/"/\\"/g')
-
-  echo "Replacing: $key -> $value"
-
-  # Replace placeholders in index.html
-  sed -i "s|__${key}__|${value}|g" "$INDEX_HTML"
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2- | sed -e 's/[\/&]/\\&/g' -e 's/"/\\"/g')
+    
+    echo "Replacing: $key -> $value"
+    
+    # Replace placeholders in index.html
+    sed -i "s|__${key}__|${value}|g" "$INDEX_HTML"
 done
 
 echo 'Environment variables injected successfully!'
