6de0ae29063b6fc8a51f87e9b86561af40ed234f
ccna123 <nguyenthanh24494@gmail.com>
2025-03-09 18:56:58 +0900

    update .env.example, docker-compose-dev.yml and inject_env.sh

--- ui/inject_env.sh
+++ ui/inject_env.sh
@@ -12,9 +12,9 @@ fi
 for i in $(env | grep REACT_APP_); do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2- | sed -e 's/[\/&]/\\&/g' -e 's/"/\\"/g')
-    
+
     echo "Replacing: $key -> $value"
-    
+
     # Replace placeholders in index.html
     sed -i "s|__${key}__|${value}|g" "$INDEX_HTML"
 done
